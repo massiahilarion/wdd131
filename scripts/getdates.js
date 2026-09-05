0b54b9e543075fe23f4e41eec7676bceeeed701a
Sunday Isaac Udoh <paulhilarion01@gmail.com>
2026-09-05 16:02:58 +0100

    "Commit"

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,9 @@
 window.addEventListener("DOMContentLoaded", () => {
+    const currentYearElement = document.getElementById("currentYear");
+    if (currentYearElement) {
+        currentYearElement.textContent = new Date().getFullYear();
+    }
+
     const lastModifiedElement = document.getElementById("lastModified");
     if (lastModifiedElement) {
         lastModifiedElement.textContent = document.lastModified;
